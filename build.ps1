a9679b6006006ba1a26e792fc1ae3bff13a1561e
Jacek Burzynski <jburzyn@gmail.com>
2017-02-23 00:40:23 +0100

    removed unused commands from build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,3 @@
-# solution-wide
-
 msbuild .\src\TypeGen /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
@@ -8,25 +6,3 @@ if ($lastExitCode -ne 0) { exit $lastExitCode }
 
 msbuild .\src\TypeGen /p:Configuration=Release /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-# restore
-
-#msbuild .\src\TypeGen\TypeGen.Cli\TypeGen.Cli.csproj /t:restore /p:TargetFramework=net46 /verbosity:minimal
-#if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-#msbuild .\src\TypeGen\TypeGen.Core\TypeGen.Core.csproj /t:restore /p:TargetFramework=netstandard1.3 /verbosity:minimal
-#if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-#msbuild .\src\TypeGen\TypeGen.Test\TypeGen.Test.csproj /t:restore /p:TargetFramework=net46 /verbosity:minimal
-#if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-# build
-
-#msbuild .\src\TypeGen\TypeGen.Cli\TypeGen.Cli.csproj /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
-#if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-#msbuild .\src\TypeGen\TypeGen.Core\TypeGen.Core.csproj /p:TargetFramework=netstandard1.3 /p:Configuration=Release /verbosity:minimal
-#if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-#msbuild .\src\TypeGen\TypeGen.Test\TypeGen.Test.csproj /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
-#if ($lastExitCode -ne 0) { exit $lastExitCode }
