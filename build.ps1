44afb540f88ef61b76415007cd47a51427710229
Jacek Burzynski <jburzyn@gmail.com>
2017-02-23 00:31:35 +0100

    build.ps1 update (t2)

--- build.ps1
+++ build.ps1
@@ -3,30 +3,30 @@
 msbuild .\src\TypeGen /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-msbuild .\src\TypeGen /t:restore /p:TargetFramework=net46 /verbosity:minimal
+msbuild .\src\TypeGen /t:restore /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-msbuild .\src\TypeGen /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
+msbuild .\src\TypeGen /p:Configuration=Release /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
 # restore
 
-msbuild .\src\TypeGen\TypeGen.Cli\TypeGen.Cli.csproj /t:restore /p:TargetFramework=net46 /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
+#msbuild .\src\TypeGen\TypeGen.Cli\TypeGen.Cli.csproj /t:restore /p:TargetFramework=net46 /verbosity:minimal
+#if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-msbuild .\src\TypeGen\TypeGen.Core\TypeGen.Core.csproj /t:restore /p:TargetFramework=netstandard1.3 /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
+#msbuild .\src\TypeGen\TypeGen.Core\TypeGen.Core.csproj /t:restore /p:TargetFramework=netstandard1.3 /verbosity:minimal
+#if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-msbuild .\src\TypeGen\TypeGen.Test\TypeGen.Test.csproj /t:restore /p:TargetFramework=net46 /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
+#msbuild .\src\TypeGen\TypeGen.Test\TypeGen.Test.csproj /t:restore /p:TargetFramework=net46 /verbosity:minimal
+#if ($lastExitCode -ne 0) { exit $lastExitCode }
 
 # build
 
-msbuild .\src\TypeGen\TypeGen.Cli\TypeGen.Cli.csproj /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
+#msbuild .\src\TypeGen\TypeGen.Cli\TypeGen.Cli.csproj /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
+#if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-msbuild .\src\TypeGen\TypeGen.Core\TypeGen.Core.csproj /p:TargetFramework=netstandard1.3 /p:Configuration=Release /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
+#msbuild .\src\TypeGen\TypeGen.Core\TypeGen.Core.csproj /p:TargetFramework=netstandard1.3 /p:Configuration=Release /verbosity:minimal
+#if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-msbuild .\src\TypeGen\TypeGen.Test\TypeGen.Test.csproj /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
+#msbuild .\src\TypeGen\TypeGen.Test\TypeGen.Test.csproj /p:TargetFramework=net46 /p:Configuration=Release /verbosity:minimal
+#if ($lastExitCode -ne 0) { exit $lastExitCode }
