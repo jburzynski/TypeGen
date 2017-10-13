ed8e505c0712e090ee1224faf495c47dd5e3244e
Jacek Burzynski <jburzyn@gmail.com>
2017-10-13 22:58:03 +0200

    added nuget-related scripts & instructions

---
+++ nuget-update.ps1
@@ -0,0 +1,44 @@
+#nuget
+
+#tools
+
+rm -Recurse -Force nuget\tools\runtimes
+copy -Recurse src\TypeGen\TypeGen.Cli\bin\Release\PublishOutput\* nuget\tools
+rm nuget\tools\TypeGen.Cli.pdb
+rm nuget\tools\TypeGen.Core.pdb
+rm nuget\tools\TypeGen.Core.xml
+
+#lib
+
+#netstandard1.3
+copy src\TypeGen\TypeGen.Core\bin\Release\netstandard1.3\TypeGen.Core.dll nuget\lib\netstandard1.3
+copy src\TypeGen\TypeGen.Core\bin\Release\netstandard1.3\TypeGen.Core.xml nuget\lib\netstandard1.3
+
+#netstandard2.0
+copy src\TypeGen\TypeGen.Core\bin\Release\netstandard2.0\TypeGen.Core.dll nuget\lib\netstandard2.0
+copy src\TypeGen\TypeGen.Core\bin\Release\netstandard2.0\TypeGen.Core.xml nuget\lib\netstandard2.0
+
+nuget pack nuget\TypeGen.nuspec
+move TypeGen.1.5.10.nupkg nuget -force
+
+if (Test-Path "local-nuget-path.txt") {
+  $localNuGetPath = Get-Content "local-nuget-path.txt"
+  copy nuget\TypeGen.1.5.10.nupkg $localNuGetPath
+}
+
+
+#chocolatey
+
+#rm -Recurse -Force chocolatey\runtimes
+#copy -Recurse src\TypeGen\TypeGen.Cli\bin\Release\PublishOutput\* chocolatey
+#rm chocolatey\TypeGen.Cli.pdb
+#rm chocolatey\TypeGen.Core.pdb
+#rm chocolatey\TypeGen.Core.xml
+
+#nuget pack chocolatey\TypeGen.nuspec
+#move TypeGen.1.5.10.nupkg chocolatey -force
+
+#if (Test-Path "local-chocolatey-path.txt") {
+#  $localChocolateyPath = Get-Content "local-chocolatey-path.txt"
+#  #copy chocolatey\TypeGen.1.5.10.nupkg $localChocolateyPath
+#}
---
+++ version-update.ps1
@@ -0,0 +1,34 @@
+if (-not ($args.length -eq 2)) {
+  Write-Host "Usage: ./version-update [old-version] [new-version]"
+  exit
+}
+
+$versionRegex = "\d+\.\d+\.\d+"
+
+$oldVersion = $args[0]
+$newVersion = $args[1]
+
+if (-not ($oldVersion -match $versionRegex) -or  -not ($newVersion -match $versionRegex)) {
+  Write-Host "Wrong version format. Should be: $($versionRegex)"
+  exit
+}
+
+# replace files' contents
+
+$paths = "nuget-update.ps1",
+         "nuget\TypeGen.nuspec",
+		 "chocolatey\TypeGen.nuspec",
+         "src\TypeGen\TypeGen.Cli\AppConfig.cs",
+         "..\TypeGenDocs\source\conf.py"
+
+foreach ($path in $paths) {
+  if (Test-Path $path) {
+    (Get-Content $path).Replace($oldVersion, $newVersion) | Set-Content $path
+  }
+}
+
+# remove old NuGet package
+
+if (Test-Path "nuget\TypeGen.$($oldVersion).nupkg") {
+  rm "nuget\TypeGen.$($oldVersion).nupkg"
+}
