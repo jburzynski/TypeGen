c8c7d9e49f14caf9d670d0519fb1ae9a2a36ca66
Jacek Burzynski <jburzyn@gmail.com>
2018-07-09 20:17:01 +0200

    trello tests-14 core tests - type annotations (some of them)

---
+++ src/TypeGen/TypeGen.Core.Test/TypeAnnotations/TsTypeAttributeTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeGen.Core.TypeAnnotations;
+using Xunit;
+
+namespace TypeGen.Core.Test.TypeAnnotations
+{
+    public class TsTypeAttributeTest
+    {
+        [Theory]
+        [InlineData(TsType.Any, "any")]
+        [InlineData(TsType.Boolean, "boolean")]
+        [InlineData(TsType.Date, "Date")]
+        [InlineData(TsType.Number, "number")]
+        [InlineData(TsType.Object, "Object")]
+        [InlineData(TsType.String, "string")]
+        public void Constructor_TsTypeGiven_MapsToCorrectTypeName(TsType type, string expectedTypeName)
+        {
+            var attribute = new TsTypeAttribute(type);
+            Assert.Equal(expectedTypeName, attribute.TypeName);
+        }
+
+        [Theory]
+        [InlineData("MyClass<string>", "MyClass")]
+        [InlineData("MyClass<string, int>", "MyClass")]
+        [InlineData("MyClass<string, int, bool>", "MyClass")]
+        [InlineData("MyClass[]", "MyClass")]
+        [InlineData("MyClass[][]", "MyClass")]
+        [InlineData("MyClass[][][][]", "MyClass")]
+        public void FlatTypeName_TypeNameGiven_ReturnsCorrectFlatTypeName(string originalTypeName, string expectedFlatTypeName)
+        {
+            var attribute = new TsTypeAttribute(originalTypeName);
+            Assert.Equal(expectedFlatTypeName, attribute.FlatTypeName);
+        }
+    }
+}
