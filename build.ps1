92cc79ceedf9630cc9b87aa9ff04a656fd589443
Jacek Burzynski <jburzyn@gmail.com>
2018-08-13 21:04:42 +0200

    removed build.ps1

--- build.ps1
+++
@@ -1,8 +0,0 @@
-msbuild .\src\TypeGen /t:clean /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-msbuild .\src\TypeGen /t:restore /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
-
-msbuild .\src\TypeGen /p:Configuration=Release /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
