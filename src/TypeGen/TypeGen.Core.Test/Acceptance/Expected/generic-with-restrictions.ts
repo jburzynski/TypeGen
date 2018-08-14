e930314b22d51294eca1e55dc3661ff44a892f30
Jacek Burzynski <jburzyn@gmail.com>
2018-08-14 20:58:17 +0200

    build fix: added generic-with-restrictions.ts

---
+++ src/TypeGen/TypeGen.Core.Test/Acceptance/Expected/generic-with-restrictions.ts
@@ -0,0 +1,10 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { TestInterface } from "./test-interfaces/test-interface";
+
+export class GenericWithRestrictions<T extends TestInterface> {
+
+}
