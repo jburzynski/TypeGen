e8f2c3f52675a7112a1013ccedc21b0fd7b6c91e
Jacek Burzynski <jburzyn@gmail.com>
2018-12-27 12:31:11 +0100

    merged build-publish.ps1 into nuget-update.ps1

--- build-publish.ps1
+++
@@ -1,8 +0,0 @@
-dotnet clean .\src\TypeGen\TypeGen.Core
-dotnet restore .\src\TypeGen\TypeGen.Core
-dotnet build .\src\TypeGen\TypeGen.Core -f netstandard1.3 -c Release
-dotnet build .\src\TypeGen\TypeGen.Core -f netstandard2.0 -c Release
-
-dotnet clean .\src\TypeGen\TypeGen.Cli
-dotnet restore .\src\TypeGen\TypeGen.Cli
-dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f netcoreapp2.1
