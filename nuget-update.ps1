ff5533d50bca283886df5d1e8920d4af54fd0660
Jacek Burzynski <jburzyn@gmail.com>
2018-12-27 13:15:22 +0100

    re-created build-publish.ps1, updated Release steps.txt

--- nuget-update.ps1
+++ nuget-update.ps1
@@ -1,17 +1,3 @@
-# build & publish
-
-dotnet clean .\src\TypeGen\TypeGen.Core
-dotnet restore .\src\TypeGen\TypeGen.Core
-dotnet build .\src\TypeGen\TypeGen.Core -f netstandard1.3 -c Release
-dotnet build .\src\TypeGen\TypeGen.Core -f netstandard2.0 -c Release
-
-dotnet clean .\src\TypeGen\TypeGen.Cli
-dotnet restore .\src\TypeGen\TypeGen.Cli
-dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f netcoreapp2.1
-
-
-
-
 #nuget
 
 #tools
