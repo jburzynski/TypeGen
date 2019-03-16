9523192457811dc2c4488ff8b55370ca30c26a15
Ion Dormenco <idormenco@gmail.com>
2019-03-16 13:44:27 +0200

    Refactor TgConfig to use a builder

---
+++ src/TypeGen/TypeGen.Cli/Models/DefaultParamsMerger.cs
@@ -0,0 +1,277 @@
+﻿using System.Linq;
+using TypeGen.Cli.Extensions;
+using TypeGen.Core;
+using TypeGen.Core.Extensions;
+
+namespace TypeGen.Cli.Models
+{
+	internal class DefaultParamsMerger
+	{
+		private readonly TgConfig _config;
+
+		private DefaultParamsMerger(TgConfig config)
+		{
+			_config = config;
+		}
+
+		public static DefaultParamsMerger InstanceFor(TgConfig config)
+		{
+			return new DefaultParamsMerger(config);
+		}
+
+		public DefaultParamsMerger WithAssembliesOrDefault()
+		{
+			if (_config.Assemblies == null)
+			{
+				_config.Assemblies = new string[0];
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithGenerationSpecsOrDefault()
+		{
+			if (_config.GenerationSpecs == null)
+			{
+				_config.GenerationSpecs = new string[0];
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithExplicitPublicAccessorOrDefault()
+		{
+			if (_config.ExplicitPublicAccessor == null)
+			{
+				_config.ExplicitPublicAccessor = GeneratorOptions.DefaultExplicitPublicAccessor;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithSingleQuotesOrDefault()
+		{
+			if (_config.SingleQuotes == null)
+			{
+				_config.SingleQuotes = GeneratorOptions.DefaultSingleQuotes;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithDefaultAddFilesToProjectOrDefault()
+		{
+			if (_config.AddFilesToProject == null)
+			{
+				_config.AddFilesToProject = TgConfig.DefaultAddFilesToProject;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithTypeScriptFileExtensionOrDefault()
+		{
+			if (_config.TypeScriptFileExtension == null)
+			{
+				_config.TypeScriptFileExtension = GeneratorOptions.DefaultTypeScriptFileExtension;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithTabLengthOrDefault()
+		{
+			if (_config.TabLength == null)
+			{
+				_config.TabLength = GeneratorOptions.DefaultTabLength;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithFileNameConvertersOrDefault()
+		{
+			if (_config.FileNameConverters == null)
+			{
+				_config.FileNameConverters = GeneratorOptions.DefaultFileNameConverters.GetTypeNames().ToArray();
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithTypeNameConvertersOrDefault()
+		{
+			if (_config.TypeNameConverters == null)
+			{
+				_config.TypeNameConverters = GeneratorOptions.DefaultTypeNameConverters.GetTypeNames().ToArray();
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithPropertyNameConvertersOrDefault()
+		{
+			if (_config.PropertyNameConverters == null)
+			{
+				_config.PropertyNameConverters = GeneratorOptions.DefaultPropertyNameConverters.GetTypeNames().ToArray();
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithEnumValueNameConvertersOrDefault()
+		{ 
+			if (_config.EnumValueNameConverters == null)
+			{
+				_config.EnumValueNameConverters = GeneratorOptions.DefaultEnumValueNameConverters.GetTypeNames().ToArray();
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithEnumStringInitializersConvertersOrDefault()
+		{
+			if (_config.EnumStringInitializersConverters == null)
+			{
+				_config.EnumStringInitializersConverters = GeneratorOptions.DefaultEnumStringInitializersConverters.GetTypeNames().ToArray();
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithExternalAssemblyPathsOrDefault()
+		{
+			if (_config.ExternalAssemblyPaths == null)
+			{
+				_config.ExternalAssemblyPaths = new string[0];
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithCreateIndexFileOrDefault()
+		{
+			if (_config.CreateIndexFile == null)
+			{
+				_config.CreateIndexFile = GeneratorOptions.DefaultCreateIndexFile;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithStrictNullChecksOrDefault()
+		{
+			if (_config.StrictNullChecks == null)
+			{
+				_config.StrictNullChecks = GeneratorOptions.DefaultStrictNullChecks;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithCsNullableTranslationOrDefault()
+		{ 
+			if (_config.CsNullableTranslation == null)
+			{
+				_config.CsNullableTranslation = GeneratorOptions.DefaultCsNullableTranslation.ToFlagString();
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithOutputPathOrDefault()
+		{
+			if (_config.OutputPath == null)
+			{
+				_config.OutputPath = string.Empty;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithClearOutputDirectoryOrDefault()
+		{
+			if (_config.ClearOutputDirectory == null)
+			{
+				_config.ClearOutputDirectory = false;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithDefaultValuesForTypesOrDefault()
+		{
+			if (_config.DefaultValuesForTypes == null)
+			{
+				_config.DefaultValuesForTypes = GeneratorOptions
+					.DefaultDefaultValuesForTypes
+					.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithCustomTypeMappingsOrDefault()
+		{
+			if (_config.CustomTypeMappings == null)
+			{
+				_config.CustomTypeMappings = GeneratorOptions
+					.DefaultCustomTypeMappings
+					.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithUseAttributesWithGenerationSpecOrDefault()
+		{
+			if (_config.UseAttributesWithGenerationSpec == null)
+			{
+				_config.UseAttributesWithGenerationSpec = GeneratorOptions.DefaultUseAttributesWithGenerationSpec;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithEnumStringInitializersOrDefault()
+		{
+			if (_config.EnumStringInitializers == null)
+			{
+				_config.EnumStringInitializers = GeneratorOptions.DefaultEnumStringInitializers;
+			}
+
+			return this;
+		}
+
+		public DefaultParamsMerger WithFileHeadingOrDefault()
+		{
+			if (_config.FileHeading == null)
+			{
+				_config.FileHeading = GeneratorOptions.DefaultFileHeading;
+			}
+
+			return this;
+		}
+		public DefaultParamsMerger WithGenerateFromAssembliesOrDefault()
+		{
+			// GenerateFromAssemblies should stay null if no value is provided
+			if (_config.GenerateFromAssemblies == null)
+			{
+				_config.GenerateFromAssemblies = null;
+			}
+
+			return this;
+		}
+
+		
+
+
+
+
+		public TgConfig Build()
+		{
+			return _config;
+		}
+
+	}
+}
--- src/TypeGen/TypeGen.Cli/Models/TgConfig.cs
+++ src/TypeGen/TypeGen.Cli/Models/TgConfig.cs
@@ -2,143 +2,141 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using TypeGen.Cli.Extensions;
-using TypeGen.Core;
-using TypeGen.Core.Converters;
 using TypeGen.Core.Extensions;
 
 namespace TypeGen.Cli.Models
 {
-    /// <summary>
-    /// Represents console configuration
-    /// </summary>
-    [DataContract]
-    internal class TgConfig
-    {
-        public static bool DefaultAddFilesToProject => false;
+	/// <summary>
+	/// Represents console configuration
+	/// </summary>
+	[DataContract]
+	internal class TgConfig
+	{
+		public static bool DefaultAddFilesToProject => false;
 
-        [Obsolete("Use Assemblies instead")]
-        [DataMember(Name = "assemblyPath")]
-        public string AssemblyPath { get; set; }
+		[Obsolete("Use Assemblies instead")]
+		[DataMember(Name = "assemblyPath")]
+		public string AssemblyPath { get; set; }
 
-        [DataMember(Name = "assemblies")]
-        public string[] Assemblies { get; set; }
-        
-        [DataMember(Name = "generationSpecs")]
-        public string[] GenerationSpecs { get; set; }
+		[DataMember(Name = "assemblies")]
+		public string[] Assemblies { get; set; }
 
-        [DataMember(Name = "fileNameConverters")]
-        public string[] FileNameConverters { get; set; }
+		[DataMember(Name = "generationSpecs")]
+		public string[] GenerationSpecs { get; set; }
 
-        [DataMember(Name = "typeNameConverters")]
-        public string[] TypeNameConverters { get; set; }
+		[DataMember(Name = "fileNameConverters")]
+		public string[] FileNameConverters { get; set; }
 
-        [DataMember(Name = "propertyNameConverters")]
-        public string[] PropertyNameConverters { get; set; }
+		[DataMember(Name = "typeNameConverters")]
+		public string[] TypeNameConverters { get; set; }
 
-        [DataMember(Name = "enumValueNameConverters")]
-        public string[] EnumValueNameConverters { get; set; }
-        
-        [DataMember(Name = "enumStringInitializersConverters")]
-        public string[] EnumStringInitializersConverters { get; set; }
+		[DataMember(Name = "propertyNameConverters")]
+		public string[] PropertyNameConverters { get; set; }
 
-        [DataMember(Name = "externalAssemblyPaths")]
-        public string[] ExternalAssemblyPaths { get; set; }
+		[DataMember(Name = "enumValueNameConverters")]
+		public string[] EnumValueNameConverters { get; set; }
 
-        [DataMember(Name = "typeScriptFileExtension")]
-        public string TypeScriptFileExtension { get; set; }
+		[DataMember(Name = "enumStringInitializersConverters")]
+		public string[] EnumStringInitializersConverters { get; set; }
 
-        [DataMember(Name = "tabLength")]
-        public int? TabLength { get; set; }
+		[DataMember(Name = "externalAssemblyPaths")]
+		public string[] ExternalAssemblyPaths { get; set; }
 
-        [DataMember(Name = "explicitPublicAccessor")]
-        public bool? ExplicitPublicAccessor { get; set; }
+		[DataMember(Name = "typeScriptFileExtension")]
+		public string TypeScriptFileExtension { get; set; }
 
-        [DataMember(Name = "singleQuotes")]
-        public bool? SingleQuotes { get; set; }
+		[DataMember(Name = "tabLength")]
+		public int? TabLength { get; set; }
 
-        [DataMember(Name = "addFilesToProject")]
-        public bool? AddFilesToProject { get; set; }
+		[DataMember(Name = "explicitPublicAccessor")]
+		public bool? ExplicitPublicAccessor { get; set; }
 
-        [DataMember(Name = "outputPath")]
-        public string OutputPath { get; set; }
-        
-        [DataMember(Name = "clearOutputDirectory")]
-        public bool? ClearOutputDirectory { get; set; }
+		[DataMember(Name = "singleQuotes")]
+		public bool? SingleQuotes { get; set; }
 
-        [DataMember(Name = "createIndexFile")]
-        public bool? CreateIndexFile { get; set; }
+		[DataMember(Name = "addFilesToProject")]
+		public bool? AddFilesToProject { get; set; }
 
-        [DataMember(Name = "strictNullChecks")]
-        public bool? StrictNullChecks { get; set; }
+		[DataMember(Name = "outputPath")]
+		public string OutputPath { get; set; }
 
-        [DataMember(Name = "csNullableTranslation")]
-        public string CsNullableTranslation { get; set; }
-        
-        [DataMember(Name = "defaultValuesForTypes")]
-        public Dictionary<string, string> DefaultValuesForTypes { get; set; }
-        
-        [DataMember(Name = "customTypeMappings")]
-        public Dictionary<string, string> CustomTypeMappings { get; set; }
-        
-        [DataMember(Name = "generateFromAssemblies")]
-        public bool? GenerateFromAssemblies { get; set; }
-        
-        [DataMember(Name = "useAttributesWithGenerationSpec")]
-        public bool? UseAttributesWithGenerationSpec { get; set; }
-        
-        [DataMember(Name = "enumStringInitializers")]
-        public bool? EnumStringInitializers { get; set; }
-        
-        [DataMember(Name = "fileHeading")]
-        public string FileHeading { get; set; }
+		[DataMember(Name = "clearOutputDirectory")]
+		public bool? ClearOutputDirectory { get; set; }
 
-        public TgConfig Normalize()
-        {
-            if (ExternalAssemblyPaths.Contains("<global-packages>"))
-            {
-                List<string> newExternalAssemblyPaths = ExternalAssemblyPaths.ToList();
-                newExternalAssemblyPaths.Remove("<global-packages>");
-                ExternalAssemblyPaths = newExternalAssemblyPaths.ToArray();
-            }
+		[DataMember(Name = "createIndexFile")]
+		public bool? CreateIndexFile { get; set; }
 
-            return this;
-        }
+		[DataMember(Name = "strictNullChecks")]
+		public bool? StrictNullChecks { get; set; }
 
-        public TgConfig MergeWithDefaultParams()
-        {
-            if (Assemblies == null) Assemblies = new string[0];
-            if (GenerationSpecs == null) GenerationSpecs = new string[0];
-            if (ExplicitPublicAccessor == null) ExplicitPublicAccessor = GeneratorOptions.DefaultExplicitPublicAccessor;
-            if (SingleQuotes == null) SingleQuotes = GeneratorOptions.DefaultSingleQuotes;
-            if (AddFilesToProject == null) AddFilesToProject = DefaultAddFilesToProject;
-            if (TypeScriptFileExtension == null) TypeScriptFileExtension = GeneratorOptions.DefaultTypeScriptFileExtension;
-            if (TabLength == null) TabLength = GeneratorOptions.DefaultTabLength;
-            if (FileNameConverters == null) FileNameConverters = GeneratorOptions.DefaultFileNameConverters.GetTypeNames().ToArray();
-            if (TypeNameConverters == null) TypeNameConverters = GeneratorOptions.DefaultTypeNameConverters.GetTypeNames().ToArray();
-            if (PropertyNameConverters == null) PropertyNameConverters = GeneratorOptions.DefaultPropertyNameConverters.GetTypeNames().ToArray();
-            if (EnumValueNameConverters == null) EnumValueNameConverters = GeneratorOptions.DefaultEnumValueNameConverters.GetTypeNames().ToArray();
-            if (EnumStringInitializersConverters == null) EnumStringInitializersConverters = GeneratorOptions.DefaultEnumStringInitializersConverters.GetTypeNames().ToArray();
-            if (ExternalAssemblyPaths == null) ExternalAssemblyPaths = new string[0];
-            if (CreateIndexFile == null) CreateIndexFile = GeneratorOptions.DefaultCreateIndexFile;
-            if (StrictNullChecks == null) StrictNullChecks = GeneratorOptions.DefaultStrictNullChecks;
-            if (CsNullableTranslation == null) CsNullableTranslation = GeneratorOptions.DefaultCsNullableTranslation.ToFlagString();
-            if (OutputPath == null) OutputPath = "";
-            if (ClearOutputDirectory == null) ClearOutputDirectory = false;
-            if (DefaultValuesForTypes == null) DefaultValuesForTypes = GeneratorOptions.DefaultDefaultValuesForTypes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-            if (CustomTypeMappings == null) CustomTypeMappings = GeneratorOptions.DefaultCustomTypeMappings.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-            // GenerateFromAssemblies should stay null if no value is provided
-            if (UseAttributesWithGenerationSpec == null) UseAttributesWithGenerationSpec = GeneratorOptions.DefaultUseAttributesWithGenerationSpec;
-            if (EnumStringInitializers == null) EnumStringInitializers = GeneratorOptions.DefaultEnumStringInitializers;
-            if (FileHeading == null) FileHeading = GeneratorOptions.DefaultFileHeading;
-            return this;
-        }
+		[DataMember(Name = "csNullableTranslation")]
+		public string CsNullableTranslation { get; set; }
 
-        public string[] GetAssemblies()
-        {
-            return Assemblies.IsNullOrEmpty() && !string.IsNullOrWhiteSpace(AssemblyPath) ?
-                new[] { AssemblyPath } :
-                Assemblies;
-        }
-    }
+		[DataMember(Name = "defaultValuesForTypes")]
+		public Dictionary<string, string> DefaultValuesForTypes { get; set; }
+
+		[DataMember(Name = "customTypeMappings")]
+		public Dictionary<string, string> CustomTypeMappings { get; set; }
+
+		[DataMember(Name = "generateFromAssemblies")]
+		public bool? GenerateFromAssemblies { get; set; }
+
+		[DataMember(Name = "useAttributesWithGenerationSpec")]
+		public bool? UseAttributesWithGenerationSpec { get; set; }
+
+		[DataMember(Name = "enumStringInitializers")]
+		public bool? EnumStringInitializers { get; set; }
+
+		[DataMember(Name = "fileHeading")]
+		public string FileHeading { get; set; }
+
+		public TgConfig Normalize()
+		{
+			if (ExternalAssemblyPaths.Contains("<global-packages>"))
+			{
+				List<string> newExternalAssemblyPaths = ExternalAssemblyPaths.ToList();
+				newExternalAssemblyPaths.Remove("<global-packages>");
+				ExternalAssemblyPaths = newExternalAssemblyPaths.ToArray();
+			}
+
+			return this;
+		}
+
+		public TgConfig MergeWithDefaultParams()
+		{
+			return DefaultParamsMerger
+				.InstanceFor(this)
+				.WithAssembliesOrDefault()
+				.WithGenerationSpecsOrDefault()
+				.WithExplicitPublicAccessorOrDefault()
+				.WithSingleQuotesOrDefault()
+				.WithDefaultAddFilesToProjectOrDefault()
+				.WithTypeScriptFileExtensionOrDefault()
+				.WithTabLengthOrDefault()
+				.WithFileNameConvertersOrDefault()
+				.WithTypeNameConvertersOrDefault()
+				.WithPropertyNameConvertersOrDefault()
+				.WithEnumValueNameConvertersOrDefault()
+				.WithEnumStringInitializersConvertersOrDefault()
+				.WithExternalAssemblyPathsOrDefault()
+				.WithCreateIndexFileOrDefault()
+				.WithStrictNullChecksOrDefault()
+				.WithCsNullableTranslationOrDefault()
+				.WithOutputPathOrDefault()
+				.WithClearOutputDirectoryOrDefault()
+				.WithDefaultValuesForTypesOrDefault()
+				.WithCustomTypeMappingsOrDefault()
+				.WithUseAttributesWithGenerationSpecOrDefault()
+				.WithEnumStringInitializersOrDefault()
+				.WithGenerateFromAssembliesOrDefault()
+				.Build();
+		}
+
+		public string[] GetAssemblies()
+		{
+			return Assemblies.IsNullOrEmpty() && !string.IsNullOrWhiteSpace(AssemblyPath) ?
+				new[] { AssemblyPath } :
+				Assemblies;
+		}
+	}
 }
