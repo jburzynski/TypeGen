a2818f059c15964f89dd5a31a32ac4ddc2fb3a9e
Jacek Burzynski <jburzyn@gmail.com>
2019-08-06 16:43:52 +0200

    corrected typos

--- src/TypeGen/TypeGen.Core/TypeAnnotations/ExportTsClassAttribute.cs
+++ src/TypeGen/TypeGen.Core/TypeAnnotations/ExportTsClassAttribute.cs
@@ -3,7 +3,7 @@
 namespace TypeGen.Core.TypeAnnotations
 {
     /// <summary>
-    /// Indentifies a class that a TypeScript file should be generated for
+    /// Identifies a class that a TypeScript file should be generated for
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = false)]
     public class ExportTsClassAttribute : ExportAttribute
--- src/TypeGen/TypeGen.Core/TypeAnnotations/ExportTsInterfaceAttribute.cs
+++ src/TypeGen/TypeGen.Core/TypeAnnotations/ExportTsInterfaceAttribute.cs
@@ -3,7 +3,7 @@
 namespace TypeGen.Core.TypeAnnotations
 {
     /// <summary>
-    /// Indentifies a class that a TypeScript interface file should be generated for
+    /// Identifies a class that a TypeScript interface file should be generated for
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = false)]
     public class ExportTsInterfaceAttribute : ExportAttribute
