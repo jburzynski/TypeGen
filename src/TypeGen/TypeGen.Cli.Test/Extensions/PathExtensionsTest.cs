a6a6ba56f18e893dc9a7c78ebd46aae50f5966db
Alisher Todzhiev <todjiev@gmail.com>
2021-09-16 01:05:07 +0700

    Added AbsolutePath unit test for Unix operations system

--- src/TypeGen/TypeGen.Cli.Test/Extensions/PathExtensionsTest.cs
+++ src/TypeGen/TypeGen.Cli.Test/Extensions/PathExtensionsTest.cs
@@ -13,8 +13,23 @@ namespace TypeGen.Cli.Test.Extensions
         [InlineData("my/path", @"C:\projects", @"C:\projects\my/path")]
         [InlineData(@"my\very\nested\path", @"Z:\very nested\path", @"Z:\very nested\path\my\very\nested\path")]
         [InlineData(@"C:\Program Files\program.exe", @"C:\path", @"C:\Program Files\program.exe")]
-        public void ToAbsolutePath_PathGiven_AbsolutePathReturned(string path, string currentWorkingDirectory, string expectedResult)
+        public void ToWindowsAbsolutePath_PathGiven_AbsolutePathReturned(string path, string currentWorkingDirectory, string expectedResult)
         {
+            if (!System.OperatingSystem.IsWindows()) return;
+
+            _fileSystem.GetCurrentDirectory().Returns(currentWorkingDirectory);
+            string actualResult = path.ToAbsolutePath(_fileSystem);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData("my\\path", @"/projects", @"/projects/my\path")]
+        [InlineData(@"my/very/nested/path", @"/very nested/path", @"/very nested/path/my/very/nested/path")]
+        [InlineData(@"/etc/program.dll", @"/path", @"/etc/program.dll")]
+        public void ToUnixAbsolutePath_PathGiven_AbsolutePathReturned(string path, string currentWorkingDirectory, string expectedResult)
+        {
+            if (!System.OperatingSystem.IsMacOS() && !System.OperatingSystem.IsLinux()) return;
+
             _fileSystem.GetCurrentDirectory().Returns(currentWorkingDirectory);
             string actualResult = path.ToAbsolutePath(_fileSystem);
             Assert.Equal(expectedResult, actualResult);
