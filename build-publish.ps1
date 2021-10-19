12b481af522762e04c79f015f72eda6dba4f9467
Jason <rametta@outlook.com>
2021-10-19 16:22:45 -0400

    Add support for net5.0

--- build-publish.ps1
+++ build-publish.ps1
@@ -9,3 +9,4 @@ dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f netcoreapp2.1
 dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f netcoreapp2.2
 dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f netcoreapp3.0
 dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f netcoreapp3.1
+dotnet publish .\src\TypeGen\TypeGen.Cli -c Release -f net5.0
