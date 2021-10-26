1baa3238b88fecb7209c6c7c5b59ea333975ecb7
Alisher Todzhiev <todjiev@gmail.com>
2021-10-26 19:28:17 +0700

    Revert "Nuget have been added"
    This reverts commit 1ab0d5977b671ef04a5540c3000d0eeb59f2eed4.

--- build-publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dotnet clean ./src/TypeGen/TypeGen.Core
-dotnet restore ./src/TypeGen/TypeGen.Core
-dotnet build ./src/TypeGen/TypeGen.Core -f net5.0 -c Release
-
-dotnet clean ./src/TypeGen/TypeGen.Cli
-dotnet restore ./src/TypeGen/TypeGen.Cli
-dotnet publish ./src/TypeGen/TypeGen.Cli -c Release -f net5.0
