220acab506f3b5a37988954390317e3160b7e401
Jacek Burzynski <jburzyn@gmail.com>
2023-08-02 11:41:50 +0200

    fixed exportTypesAsInterfacesByDefault and useImportType not working in tgconfig.json

--- src/TypeGen/TypeGen.Cli/Business/GeneratorOptionsProvider.cs
+++ src/TypeGen/TypeGen.Cli/Business/GeneratorOptionsProvider.cs
@@ -67,7 +67,9 @@ namespace TypeGen.Cli.Business
                 CustomTypeMappings = config.CustomTypeMappings ?? GeneratorOptions.DefaultCustomTypeMappings,
                 EnumStringInitializers = config.EnumStringInitializers ?? GeneratorOptions.DefaultEnumStringInitializers,
                 FileHeading = config.FileHeading ?? GeneratorOptions.DefaultFileHeading,
-                UseDefaultExport = config.UseDefaultExport ?? GeneratorOptions.DefaultUseDefaultExport
+                UseDefaultExport = config.UseDefaultExport ?? GeneratorOptions.DefaultUseDefaultExport,
+                ExportTypesAsInterfacesByDefault = config.ExportTypesAsInterfacesByDefault ?? GeneratorOptions.DefaultExportTypesAsInterfacesByDefault,
+                UseImportType = config.UseImportType ?? GeneratorOptions.DefaultUseImportType
             };
         }
 
