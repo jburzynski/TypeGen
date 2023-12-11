1af78d8a3419c65a264d34b904abd6e0a02763f6
Jacek Burzynski <jburzyn@gmail.com>
2023-12-11 11:45:17 +0100

    added test for #184 Interface dependencies no longer generated across multiple 'outputDir' directories; updated project URL in nuspec

---
+++ src/TypeGen/TypeGen.FileContentTest/MultipleOutputDirsWithDependencies/MultipleOutputDirsWithDependenciesTest.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using TypeGen.Core.Generator;
+using TypeGen.Core.SpecGeneration;
+using TypeGen.FileContentTest.MultipleOutputDirsWithDependencies.Entities;
+using TypeGen.FileContentTest.TestingUtils;
+using Xunit;
+
+namespace TypeGen.FileContentTest.MultipleOutputDirsWithDependencies;
+    
+public class MultipleOutputDirsWithDependenciesTest
+{
+    [Fact]
+    public async Task TestMultipleOutputDirsWithDependencies()
+    {
+        const string class1OutputBase = "./class1/output/";
+        const string class2OutputBase = "./class2/output/";
+        const string class1Output = $"{class1OutputBase}class1.ts";
+        const string class2Output = $"{class2OutputBase}class2.ts";
+        const string interface1Output = $"{class1OutputBase}interface1.ts";
+        const string interface2Output = $"{class2OutputBase}interface2.ts";
+        
+        var generationSpec = new MultipleOutputDirsWithDependenciesGenerationSpec();
+        var generatorOptions = new GeneratorOptions();
+        
+        var generator = new Generator(generatorOptions);
+        var interceptor = GeneratorOutputInterceptor.CreateInterceptor(generator);
+
+        await generator.GenerateAsync(generationSpec);
+
+        interceptor.GeneratedOutputs.Should().ContainKey(typeof(Class1));
+        interceptor.GeneratedOutputs.Should().ContainKey(typeof(Class2));
+        interceptor.GeneratedOutputs.Should().ContainKey(typeof(Interface1));
+        interceptor.GeneratedOutputs.Should().ContainKey(typeof(Interface2));
+
+        interceptor.GeneratedOutputs[typeof(Class1)].Path.Should().Be(class1Output);
+        interceptor.GeneratedOutputs[typeof(Class2)].Path.Should().Be(class2Output);
+        interceptor.GeneratedOutputs[typeof(Interface1)].Path.Should().Be(interface1Output);
+        interceptor.GeneratedOutputs[typeof(Interface2)].Path.Should().Be(interface2Output);
+    }
+
+    private class MultipleOutputDirsWithDependenciesGenerationSpec : GenerationSpec
+    {
+        public MultipleOutputDirsWithDependenciesGenerationSpec()
+        {
+            AddClass<Class1>("./class1/output");
+            AddClass<Class2>("./class2/output");
+        }
+    }
+}
--- src/TypeGen/TypeGen.FileContentTest/TestingUtils/GeneratorOutputInterceptor.cs
+++ src/TypeGen/TypeGen.FileContentTest/TestingUtils/GeneratorOutputInterceptor.cs
@@ -2,56 +2,39 @@
 using System.Collections.Generic;
 using Gen = TypeGen.Core.Generator;
 
-namespace TypeGen.FileContentTest.TestingUtils
+namespace TypeGen.FileContentTest.TestingUtils;
+
+/// <summary>
+///     Intercepts the output produced by the <see cref="Gen.Generator" />
+///     so it can be evaluated in tests
+/// </summary>
+public class GeneratorOutputInterceptor
 {
-    /// <summary>
-    /// Intercepts the output produced by the <see cref="Gen.Generator"/>
-    /// so it can be evaluated in tests
-    /// </summary>
-    public class GeneratorOutputInterceptor
+    private GeneratorOutputInterceptor()
     {
+    }
 
-        #region Constructors
+    private Dictionary<Type, GeneratedOutput> _GeneratedOutputs { get; } = new();
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="generator"></param>
-        /// <param name="disableDefaultOutput"></param>
-        /// <returns></returns>
-        public static GeneratorOutputInterceptor CreateInterceptor(Gen.Generator generator, bool disableDefaultOutput = true)
-        {
-            var interceptor = new GeneratorOutputInterceptor();
-            if (disableDefaultOutput)
-                generator.UnsubscribeDefaultFileContentGeneratedHandler();
+    public IReadOnlyDictionary<Type, GeneratedOutput> GeneratedOutputs => _GeneratedOutputs;
 
-            generator.FileContentGenerated += interceptor.OnOutputCreated;
-            return interceptor;
+    /// <summary>
+    /// </summary>
+    /// <param name="generator"></param>
+    /// <param name="disableDefaultOutput"></param>
+    /// <returns></returns>
+    public static GeneratorOutputInterceptor CreateInterceptor(Gen.Generator generator, bool disableDefaultOutput = true)
+    {
+        var interceptor = new GeneratorOutputInterceptor();
+        if (disableDefaultOutput)
+            generator.UnsubscribeDefaultFileContentGeneratedHandler();
 
-        }
+        generator.FileContentGenerated += interceptor.OnOutputCreated;
+        return interceptor;
+    }
 
-        private GeneratorOutputInterceptor()
-        {
-        }
-
-        #endregion
-
-        #region Properties
-
-
-        public IReadOnlyDictionary<Type, GeneratedOutput> GeneratedOutputs => _GeneratedOutputs;
-
-        private Dictionary<Type, GeneratedOutput> _GeneratedOutputs { get; set; } = new Dictionary<Type, GeneratedOutput>();
-
-        #endregion
-
-        #region Generator interception
-
-        private void OnOutputCreated(object sender, Gen.FileContentGeneratedArgs e)
-        {
-            _GeneratedOutputs[e.Type] = new GeneratedOutput(e);
-        }
-
-        #endregion
+    private void OnOutputCreated(object sender, Gen.FileContentGeneratedArgs e)
+    {
+        _GeneratedOutputs[e.Type] = new GeneratedOutput(e);
     }
 }
