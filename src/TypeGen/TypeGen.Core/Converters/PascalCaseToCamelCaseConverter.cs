eb05a7c73c9e31ed1daa86c80df66048ea4363e4
Chris Justesen <chris@smoerfugl.dk>
2024-03-06 20:37:50 +0100

    Fix casing on characters afterwards

--- src/TypeGen/TypeGen.Core/Converters/PascalCaseToCamelCaseConverter.cs
+++ src/TypeGen/TypeGen.Core/Converters/PascalCaseToCamelCaseConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using TypeGen.Core.Validation;
@@ -24,8 +25,36 @@ namespace TypeGen.Core.Converters
 
         private static string ConvertTypeInvariant(string name)
         {
-            char firstChar = char.ToLowerInvariant(name[0]);
-            return firstChar + name.Remove(0, 1);
+            var chars = name.ToCharArray();
+            FixCasing(chars);
+            return new string(chars);
+        }
+
+        private static void FixCasing(IList<char> chars)
+        {
+            for (var i = 0; i < chars.Count; i++)
+            {
+                if (i == 1 && !char.IsUpper(chars[i]))
+                {
+                    break;
+                }
+
+                var hasNext = i + 1 < chars.Count;
+
+                // Stop when next char is already lowercase.
+                if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+                {
+                    // If the next char is a space, lowercase current char before exiting.
+                    if (chars[i + 1] == ' ')
+                    {
+                        chars[i] = char.ToLowerInvariant(chars[i]);
+                    }
+
+                    break;
+                }
+
+                chars[i] = char.ToLowerInvariant(chars[i]);
+            }
         }
     }
 }
